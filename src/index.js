a34da6528164db49c48c5e75c42a9ff6271cc9c7
Siddharth <siddharthvaibhav.8@gmail.com>
2020-05-30 16:01:55 +0530

    Added Class component Logic

--- src/index.js
+++ src/index.js
@@ -2,61 +2,132 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-var DisplayEmployeeInfo = (employee) => {
-  return (
-    <div>
-      <h1>Employee Details</h1>
-      <p>
-        <label>
-          Employee Id: <b>{employee.Id}</b>
-        </label>
-      </p>
-      <p>
-        <label>
-          Employee Name: <b>{employee.Name}</b>
-        </label>
-      </p>
-      <p>
-        <label>
-          Employee Salary: <b>{employee.Salary}</b>
-        </label>
-      </p>
-      <p>
-        <label>
-          Employee Location: <b>{employee.Location}</b>
-        </label>
-      </p>
-      <Department
-        deptName={employee.deptName}
-        headName={employee.headName}
-      ></Department>
-    </div>
-  );
-};
+class Employee extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>Employee Details....</h2>
+        <p>
+          <label>
+            Employee Id: <b>{this.props.Id}</b>
+          </label>
+        </p>
+        <p>
+          <label>
+            Employee Name: <b>{this.props.Name}</b>
+          </label>
+        </p>
+        <p>
+          <label>
+            Employee Salary: <b>{this.props.Salary}</b>
+          </label>
+        </p>
+        <p>
+          <label>
+            Employee Location: <b>{this.props.Location}</b>
+          </label>
+        </p>
+        <Department
+          deptName={this.props.deptName}
+          deptHead={this.props.deptHead}
+        ></Department>
+      </div>
+    );
+  }
+}
 
-const Department = (deptInfo) => {
-  return (
-    <div>
-      <p>
-        Department Name :<b>{deptInfo.deptName}</b>
-      </p>
-      <p>
-        Department Head Name: <b>{deptInfo.headName}</b>
-      </p>
-    </div>
-  );
-};
+class Department extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Department Details</h3>
+        <p>
+          <label>
+            Dept Name: <b>{this.props.deptName}</b>
+          </label>
+        </p>
+        <p>
+          <label>
+            Dept Head: <b>{this.props.deptHead}</b>
+          </label>
+        </p>
+      </div>
+    );
+  }
+}
+
 const element = (
-  <DisplayEmployeeInfo
+  <Employee
     Id="101"
-    Name="John F"
-    Location="New Jersey"
-    Salary="82000"
-    deptName="HR"
-    headName="Richard"
-  ></DisplayEmployeeInfo>
+    Name="John"
+    Location="NewJersey"
+    Salary="3000$"
+    deptName="BFSI"
+    deptHead="Patrick James"
+  ></Employee>
 );
+
 ReactDOM.render(element, document.getElementById("root"));
+//Functional Component
+// var DisplayEmployeeInfo = (employee) => {
+//   return (
+//     <div>
+//       <h1>Employee Details</h1>
+//       <p>
+//         <label>
+//           Employee Id: <b>{employee.Id}</b>
+//         </label>
+//       </p>
+//       <p>
+//         <label>
+//           Employee Name: <b>{employee.Name}</b>
+//         </label>
+//       </p>
+//       <p>
+//         <label>
+//           Employee Salary: <b>{employee.Salary}</b>
+//         </label>
+//       </p>
+//       <p>
+//         <label>
+//           Employee Location: <b>{employee.Location}</b>
+//         </label>
+//       </p>
+//       <Department
+//         deptName={employee.deptName}
+//         headName={employee.headName}
+//       ></Department>
+//     </div>
+//   );
+// };
+
+// const Department = (deptInfo) => {
+//   return (
+//     <div>
+//       <p>
+//         Department Name :<b>{deptInfo.deptName}</b>
+//       </p>
+//       <p>
+//         Department Head Name: <b>{deptInfo.headName}</b>
+//       </p>
+//     </div>
+//   );
+// };
+// const element = (
+//   <DisplayEmployeeInfo
+//     Id="101"
+//     Name="John F"
+//     Location="New Jersey"
+//     Salary="82000"
+//     deptName="HR"
+//     headName="Richard"
+//   ></DisplayEmployeeInfo>
+// );
+// ReactDOM.render(element, document.getElementById("root"));
 // const element = React.createElement("h1", null, "Welcome To React Programming");
 // ReactDOM.render(element, document.getElementById("root"));
 
